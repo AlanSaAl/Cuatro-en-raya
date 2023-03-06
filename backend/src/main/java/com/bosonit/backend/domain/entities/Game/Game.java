e657f64e005d6a06783ca37b321efaab01906c04
juanjg97 <juanjosejg2711@gmail.com>
2023-03-06 08:18:07 -0600

    Se agrega docker-compose.yml para trabajar con POSTGRES

--- backend/src/main/java/com/bosonit/backend/domain/entities/Game/Game.java
+++ backend/src/main/java/com/bosonit/backend/domain/entities/Game/Game.java
@@ -4,27 +4,27 @@ import com.bosonit.backend.domain.entities.Player.Player;
 import lombok.*;
 
 import javax.persistence.*;
+import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 
 @Entity
-@Table(name="game")
+@Table(name="games")
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
 @ToString
 @Builder
-public class Game {
+public class Game implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     @Column(name = "id_game", nullable = false)
     public Integer idGame;
 
-    @Column(name = "games")
+    @Column(name = "game")
     @ManyToMany(mappedBy = "games",fetch = FetchType.EAGER)
     private List<Player> players = new ArrayList<Player>();
-
 
     @ManyToOne(fetch = FetchType.EAGER)
     private Player winner;
@@ -44,4 +44,10 @@ public class Game {
     public void addShot(Shot shot){
         shots.add(shot);
     }
+
+    public static long getSerialversionuid() {
+        return serialVersionUID;
+    }
+
+    private static final long serialVersionUID = 1L;
 }
